999ec95af528312cfb1c68b042b56a2ad1312195
Bapi <bapiraju2007@gmail.com>
2016-11-21 22:08:57 -0500

    ADC driver changes

--- device/adc/adcread.c
+++ device/adc/adcread.c
@@ -4,14 +4,18 @@ devcall adcread(struct dentry *devptr,
     		  char *buff, 
     		  int32 blk) {
 	
-	//struct adc_csreg * adcptr;
-	uint32 * adcptr;
-	//adcptr = (struct adc_csreg *)devptr->dvcsr;
-	//adcptr = (uint32 *)();
-	//gptr->oer &= (0xFFFFFFFF ^ (1 << 28));
-        //gptr->data_out |= (1 << 28);
-	adcptr = (uint32*)(STEP_ENABLE);
-	*adcptr |= (1 << 2);
-	kprintf("Reached adc read: %d", *adcptr);
+	uint32 *adc_tsc_clkptr, *ctrlptr;
+	
+	adc_tsc_clkptr = (uint32*)(0x44E004BC);
+	kprintf("Reading WKUP_ADC_TSC_CLKCTRL register value: %d\n", *adc_tsc_clkptr);
+	*adc_tsc_clkptr |= 0x02;
+	sleep(1);
+	kprintf("Updated WKUP_ADC_TSC_CLKCTRL register value: %d\n", *adc_tsc_clkptr);
+
+	ctrlptr = (uint32*)(ADC_CTRL);
+	kprintf("Reading ADC_CTRL register value: %d\n", *ctrlptr);
+	*ctrlptr |= (0x01<<2);
+	kprintf("Updated ADC_CTRL register value: %d\n", *ctrlptr);
+	
 	return 1;
 }
--- include/adc.h
+++ include/adc.h
@@ -3,44 +3,9 @@ struct  adc_csreg
         volatile uint32 data1;
 };
 
-/*struct  uart_csreg
-{
-        volatile uint32 buffer; // receive buffer (when read)           
-                                //   OR transmit hold (when written)    
-        volatile uint32 ier;    // interrupt enable                     
-        volatile uint32 iir;    // interrupt identification (when read) 
-                                //   OR FIFO control (when written)     
-        volatile uint32 lcr;    // line control register                
-        volatile uint32 mcr;    // modem control register               
-        volatile uint32 lsr;    // line status register                 
-        volatile uint32 msr;    // modem status register                
-        volatile uint32 spr;    // scratch register                     
-        volatile uint32 mdr1;
-        volatile uint32 res[12];// unused UART registers                
-        volatile uint32 sysc;   // system configuration register        
-        volatile uint32 syss;   // system status register               
-        volatile uint32 wer;
-        volatile uint32 res4;
-        volatile uint32 rxfifo_lvl;
-        volatile uint32 txfifo_lvl;
-        volatile uint32 ier2;
-        volatile uint32 isr2;
-        volatile uint32 freq_sel;
-        volatile uint32 res5[2];
-        volatile uint32 mdr3;
-        volatile uint32 tx_dma_thresh;
-};*/
-
-/* Alternative names for control and status registers */
-/*
-#define rbr     buffer          // receive buffer (when read)           
-#define thr     buffer          // transmit hold (when written)         
-#define fcr     iir             // FIFO control (when written)          
-#define dll     buffer          // divisor latch (low byte)             
-#define dlm     ier             // divisor latch (high byte)            
-*/
-
 /* Definintion of individual bits in control and status registers       */
-#define STEP_ENABLE	0x44E0D054
-#define STEP_CONFIG	0x44E0D06C
-#define STEP_DELAY	0x44E0D070
+#define WKUP_ADC_TSC_CLKCTRL	0x44E004BC
+#define ADC_CTRL		0x44E0D040
+#define STEP_ENABLE		0x44E0D054
+#define STEP_CONFIG		0x44E0D06C
+#define STEP_DELAY		0x44E0D070
